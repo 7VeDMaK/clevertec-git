038f1d2935009e11b875c5feed381aff78b2a1fd
Balay D <darvinbin@gmail.com>
2024-08-06 17:34:13 +0300

    added final feature

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //Last change before something in develop branch
-        //New feature 1 was changed
-        //New feature for the new feature
+        //We closing!
     }
 }
