28061e3f20703c87fe71572db98a424f733999ab
Balay D <darvinbin@gmail.com>
2024-08-06 14:45:19 +0300

    Major feature for next release

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //Last change before something in develop branch
+        //Global feature 2
     }
 }
